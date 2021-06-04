9c08ad52099275edefd43c01bfb359505ed3d17e
Marlon Strong <tsenane14@gmail.com>
2021-06-05 01:29:20 +0200

    added statusbarPage changes

--- shlink/src/components/statusBar/index.js
+++ shlink/src/components/statusBar/index.js
@@ -1,10 +1,11 @@
+import { useIsFocused } from '@react-navigation/core'
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function StatusBarPage() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+export default function StatusBarPage(props) {
+
+    const isFocused= useIsFocused();
+
+
+    return isFocused ? <StatusBarPage {...props} /> : null
 }
