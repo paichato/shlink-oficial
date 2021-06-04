49a78c77d8001d7c6df02221ed59fec5d84c0f4e
Marlon Strong <tsenane14@gmail.com>
2021-06-05 01:25:45 +0200

    added statusbarPage

---
+++ shlink/src/components/statusBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function StatusBarPage() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
