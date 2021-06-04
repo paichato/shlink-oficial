f1d30a98f5cc8df3f5dd0a4d62f3c6437a343e61
Marlon Strong <tsenane14@gmail.com>
2021-06-05 01:57:34 +0200

    added openDrawer

--- shlink/src/components/menu/index.js
+++ shlink/src/components/menu/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { ButtonMenu } from './styles'
 import {Feather} from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/core'
 
 export default function Menu() {
+
+    const navigation=useNavigation();
+
     return (
        
-            <ButtonMenu>
+            <ButtonMenu onPress={()=>navigation.openDrawer()} >
                 <Feather name="menu" size={40} color="#fff"/>
             </ButtonMenu>
       
