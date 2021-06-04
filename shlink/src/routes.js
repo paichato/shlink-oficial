06b3cdf4dc89845fdb9fb7b3a048b18399be8c5c
Marlon Strong <tsenane14@gmail.com>
2021-06-05 01:04:25 +0200

    added icon to drawer

--- shlink/src/routes.js
+++ shlink/src/routes.js
@@ -4,12 +4,28 @@ import {createDrawerNavigator} from '@react-navigation/drawer'
 import Home from './screens/Home'
 import MyLinks from './screens/MyLinks'
 
+import {Ionicons} from '@expo/vector-icons'
+
 const Drawer=createDrawerNavigator();
 
 function Routes(){
     return(
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home"  component={Home}/>
+        <Drawer.Navigator
+        drawerContentOptions={{
+            activeBackgroundColor: '#2ccbb9',
+            activeTintColor: '#fff',
+            marginTop:16,
+            labelStyle:{
+                fontSize:20,
+            }
+        }}
+        >
+            <Drawer.Screen name="Home"  component={Home} options={{
+                title:'Encurtar Links',
+                drawerIcon:({focused,size,color})=>(
+                    <Ionicons name="cube" color={color} size={size}/>
+                )
+                }}/>
             <Drawer.Screen name="MyLinks" component={MyLinks}/>
         </Drawer.Navigator>
     )
