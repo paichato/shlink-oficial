ac3edc5f31a76a64990b10b6efff2c20798e4cfe
Marlon Strong <tsenane14@gmail.com>
2021-06-05 01:22:53 +0200

    added linear gradient

--- shlink/src/screens/Home/index.js
+++ shlink/src/screens/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function Home() {
     return (
-        <View>
+        <LinearGradient style={{flex:1,justifyContent:'center'}} colors={['#89f7fe','#66a6ff']}>
             <Text>Home</Text>
-        </View>
+        </LinearGradient>
     )
 }
