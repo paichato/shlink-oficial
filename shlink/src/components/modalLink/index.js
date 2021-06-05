3beb3d1e190168a4f0bf47396d6eee41aa4ab893
Marlon Strong <tsenane14@gmail.com>
2021-06-05 17:59:52 +0200

    add feather icons modallink

--- shlink/src/components/modalLink/index.js
+++ shlink/src/components/modalLink/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import ModalContainer from './styles'
+import { View, Text, TouchableOpacity } from 'react-native'
+import ModalContainer, { Container, Header } from './styles'
+import {Feather} from '@expo/vector-icons'
 
 
 export default function ModalLink() {
     return (
         <ModalContainer>
+            <Container>
+                <Header>
+                    <TouchableOpacity>
+                        <Feather name="x" color="#274143" size={30}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Feather name="share" color="#274143" size={30}/>
+                    </TouchableOpacity>
 
+                </Header>
+            </Container>
         </ModalContainer>
     )
 }
